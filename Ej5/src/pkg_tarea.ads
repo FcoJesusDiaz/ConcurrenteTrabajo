bccbb7db949b41a7450a2a566d77bd149b665bb5
FcoJesusDiaz <FcoJesus.Diaz@alu.uclm.es>
2021-05-05 21:01:27 +0200

    Reto 4 superado

---
+++ Ej5/src/pkg_tarea.ads
@@ -0,0 +1,9 @@
+with Ada.Real_Time; use Ada.Real_Time;
+with Ada.Text_IO; use Ada.Text_IO;
+package tareas is
+    procedure A;
+    procedure B;
+    procedure C;
+    procedure D;
+    procedure E;
+end tareas;
